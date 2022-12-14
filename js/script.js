a61d639a501fdc83f88d56701a8aeb37ad8e88d1
Sebastian-ivaniczki <sivaniczki@gmail.com>
2022-12-14 14:31:36 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*
+Data una lista della spesa (inventatela), 
+stampare in pagina gli elementi della lista individualmente 
+con un ciclo while.  (anche brutalmente, basta che si vedano)
+*/
+
+//prendo gli elementi in pagina
+
+const target = document.getElementById('target');
+//creo un aray per gli elementi della speas 
+const shopingList = ['uova', 'latte', 'burro', 'mele'];
+//prendo la lunghezza del arai
+const elementNumber = shopingList.length
+
+console.log(elementNumber); 
+
+//creo variabile d'appoggio
+let element = ''
+let i = 0;
+while(i < elementNumber){
+    element += `<li>${shopingList[i]}</li>`
+    i++
+    //console.log(element)
+}
+
+
+target.innerHTML += element
+
